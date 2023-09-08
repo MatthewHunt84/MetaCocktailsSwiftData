0796879cba4e3e9d0b056b53b54c879778ce5ac0
MatthewHunt84 <matthew.hunt@wundermanthompson.com>
2023-09-07 20:17:32 -0600

    Initial Commit

---
+++ MetaCocktailsSwiftData/MetaCocktailsSwiftDataApp.swift
@@ -0,0 +1,32 @@
+//
+//  MetaCocktailsSwiftDataApp.swift
+//  MetaCocktailsSwiftData
+//
+//  Created by Matt Hunt on 9/7/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MetaCocktailsSwiftDataApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
