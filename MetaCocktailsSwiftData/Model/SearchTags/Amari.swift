25289d30dbfc33e4e3f9251cd008d2a5b0681691
James Menkal <jamesmenkal@Jamess-MacBook-Pro.local>
2023-12-09 16:36:13 -0700

    created Nav Links for search results view

---
+++ MetaCocktailsSwiftData/Model/SearchTags/Amari.swift
@@ -0,0 +1,21 @@
+//
+//  Amari.swift
+//  MetaCocktailsSwiftData
+//
+//  Created by James Menkal on 12/9/23.
+//
+
+import Foundation
+
+enum Amari: String, Codable, CaseIterable {
+    
+    case aperol               = "Aperol"
+    case amaroMontenegro      = "Amaro Montenegro"
+    case amaroNonino          = "Amaro Nonino"
+    case becherovka           = "Becherovka"
+    case campari              = "Campari"
+    case fernetBranca         = "Fernet Branca"
+    case fernetBrancaMenta    = "Fernet Branca Menta"
+    case suze                 = "Suze"
+    
+}
---
+++ MetaCocktailsSwiftData/Model/SearchTags/Wine.swift
@@ -0,0 +1,15 @@
+//
+//  Wine.swift
+//  MetaCocktailsSwiftData
+//
+//  Created by James Menkal on 12/9/23.
+//
+
+import Foundation
+
+enum Wine: String, Codable, CaseIterable {
+    
+    case champagne            = "Chilled Champagne"
+    case prosecco             = "Chilled Prosecco"
+    
+}
