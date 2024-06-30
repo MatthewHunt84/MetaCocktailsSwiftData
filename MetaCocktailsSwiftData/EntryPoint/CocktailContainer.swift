48ec4ca153b33a01a0236372ae39b3c84c2b1f0b
James Menkal <gojamesgo85@gmail.com>
2024-06-30 15:58:19 -0600

    Converted vodka objects for testing

--- MetaCocktailsSwiftData/EntryPoint/CocktailContainer.swift
+++ MetaCocktailsSwiftData/EntryPoint/CocktailContainer.swift
@@ -5,7 +5,7 @@
 //  Created by Matt Hunt on 2/17/24.
 //
 
-import Foundation
+import SwiftUI
 import SwiftData
 
 
@@ -21,12 +21,47 @@ actor CocktailContainer {
             if shouldPreload {
                 print("🕔🕔🕔 PRELOADING COCKTAILS FOR FIRST TIME LAUNCH 🕔🕔🕔")
                 
-//                container.mainContext.insert(aloeForThatBurn)
-
+                    // container.mainContext.insert(aloeForThatBurn)
+                
                 let _ = Preload.allCases.map { $0.cocktails }
-                                        .flatMap { $0 }
-                                        .map { container.mainContext.insert($0) }
-// let newModelArray = createNewModelArray()
+                    .flatMap { $0 }
+                    .map { container.mainContext.insert($0) }
+//                let newModelArray: [Ingredient] = createNewModelArray()
+                
+                let allCocktails = Preload.allCases.map { $0.cocktails }
+                    .flatMap { $0 }
+                
+                
+                
+                var newIngredients: [Ingredient] = []
+                for cocktail in allCocktails {
+                    
+                    for ingredient in cocktail.spec {
+                        
+                        if ingredient.ingredient.category == "Vodka"{
+                            
+                            //                let category = Switch ingredient type to get category and name. The switch will only have one case
+                            // I couldn't figure out how to do the switch statement on IngredientType. 😔
+                            
+                            
+                            
+                            newIngredients.append(Ingredient(ingredient.ingredient.name,
+                                                             ingredientCategory: IngredientCategory.vodkas,
+                                                             tagsWithSubcategories: ingredient.ingredient.tags,
+                                                             value: ingredient.value,
+                                                             unit: ingredient.unit,
+                                                             prep: ingredient.prep))
+                            
+                        }
+                    }
+                    
+                    
+                    
+                }
+                for ingredient in newIngredients {
+                    print("Ingredient is \(ingredient.name), value is \(ingredient.value)")
+                }
+                
                 shouldPreload = false
             } else {
                 print("✅✅✅ COCKTAILS LOADED FROM DATABASE ✅✅✅")
@@ -39,24 +74,46 @@ actor CocktailContainer {
         // MARK: FUNCTION FOR COCKTAIL CONVERSION
         
         
-
+        
     }
     
-    func createNewModelArray() {
-        // return an array of new model objects, but only for the first case just to make sure it works 
+    func createNewModelArray() -> [Ingredient]{
+        // return an array of new model objects, but only for the first case just to make sure it works
         // get all the cocktails (line 26)
         let allCocktails = Preload.allCases.map { $0.cocktails }
-                                .flatMap { $0 }
-                       
-        let ingredientsForCocktail = allCocktails.map { $0.spec }
+            .flatMap { $0 }
         
-        for oldIngredient in ingredientsForCocktail {
-            //ForEach map
-            //let name = name
-            // let measurement unit = measurement unit
-            // let value = value
-            // let category = Switch ingredient type to get category and name. The switch will only have one case
+        
+        
+        var newIngredients: [Ingredient] = []
+        for cocktail in allCocktails {
+            
+            for ingredient in cocktail.spec {
+                
+                if ingredient.ingredient.category == "Vodka"{
+                    
+                    //                let category = Switch ingredient type to get category and name. The switch will only have one case
+                    // I couldn't figure out how to do the switch statement on IngredientType. 😔
+                    
+                    
+                    
+                    newIngredients.append(Ingredient(ingredient.ingredient.name,
+                                                     ingredientCategory: IngredientCategory.vodkas,
+                                                     tagsWithSubcategories: ingredient.ingredient.tags,
+                                                     value: ingredient.value,
+                                                     unit: ingredient.unit,
+                                                     prep: ingredient.prep))
+                    
+                }
+            }
+            
+            
+            
+        }
+        for ingredient in newIngredients {
+            print("Ingredient is \(ingredient.name), value is \(ingredient.value)")
         }
         
+        return newIngredients
     }
 }
--- MetaCocktailsSwiftData/Model/Components/Ingredient.swift
+++ MetaCocktailsSwiftData/Model/Components/Ingredient.swift
@@ -78,15 +78,16 @@ class Ingredient: Codable, Hashable {
     let id: UUID
     let name: String
     let ingredientCategory: IngredientCategory
-    //Tags containing sub categories from old enums
-    let ingredient: IngredientType
+    let tagsWithSubcategories: Tags?
     let value: Double
     let unit: MeasurementUnit
     let prep: Prep?
     
     
-    init(_ ingredient: IngredientType, value: Double, unit: MeasurementUnit = .fluidOunces, prep: Prep? = nil) {
-        self.ingredient = ingredient
+    init(_ name: String, ingredientCategory: IngredientCategory, tagsWithSubcategories: Tags? = Tags(), value: Double, unit: MeasurementUnit = .fluidOunces, prep: Prep? = nil) {
+        self.name = name
+        self.ingredientCategory = ingredientCategory
+        self.tagsWithSubcategories = tagsWithSubcategories
         self.value = value
         self.unit = unit
         self.id = UUID()
@@ -115,13 +116,15 @@ class Ingredient: Codable, Hashable {
     // MARK: - @Model codable conformance
 
     enum CodingKeys: CodingKey {
-        case id, ingredient, value, unit, prep
+        case id, name, ingredientCategory, tagsWithSubcategories, value, unit, prep
     }
 
     required init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         self.id = try container.decode(UUID.self, forKey: .id)
-        self.ingredient = try container.decode(IngredientType.self, forKey: .ingredient)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.ingredientCategory = try container.decode(IngredientCategory.self, forKey: .ingredientCategory)
+        self.tagsWithSubcategories = try container.decode(Tags.self, forKey: .tagsWithSubcategories)
         self.value = try container.decode(Double.self, forKey: .value)
         self.unit = try container.decode(MeasurementUnit.self, forKey: .unit)
         self.prep = try container.decode(Prep.self, forKey: .prep)
@@ -131,12 +134,41 @@ class Ingredient: Codable, Hashable {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(id, forKey: .id)
         try container.encode(value, forKey: .value)
-        try container.encode(ingredient, forKey: .ingredient)
+        try container.encode(name, forKey: .name)
+        try container.encode(ingredientCategory, forKey: .ingredientCategory)
+        try container.encode(tagsWithSubcategories, forKey: .tagsWithSubcategories)
         try container.encode(unit, forKey: .unit)
         try container.encode(prep, forKey: .prep
         )
     }
 }
 
+
+
+enum IngredientCategory: String, Codable, CaseIterable  {
+    
+    case syrups            = "Syrup"
+    case juices            = "Juice"
+    case herbs             = "Herbs"
+    case fruit             = "Fruit"
+    case seasoning         = "Seasoning"
+    case soda              = "Sodas"
+    case otherNonAlc       = "Other N/A"
+    case agaves            = "Agave"
+    case brandies          = "Brandy"
+    case gins              = "Gin"
+    case otherAlcohol      = "Other Alcohol"
+    case rums              = "Rum"
+    case vodkas            = "Vodka"
+    case whiskies          = "Whiskies"
+    case liqueurs          = "Liqueurs"
+    case fortifiedWines    = "Fortified Wine"
+    case wines             = "Wine"
+    case bitters           = "Bitters"
+    case amari             = "Amari"
+    
+    
+    
+}
 
 
