55d65b488d448d3aa4604d27f3842d418be357e6
Matt Hunt <matthew.hunt@wundermanthompson.com>
2024-07-13 23:16:08 -0600

    loaded missing files

---
+++ MetaCocktailsSwiftData/Model/DummyCocktails/Brunelle.swift
@@ -0,0 +1,23 @@
+//
+//  Brunelle.swift
+//  MetaCocktailsSwiftData
+//
+//  Created by James Menkal on 1/23/24.
+//
+
+import Foundation
+
+let brunelle = Cocktail(cocktailName: "Brunelle",
+                        glasswareType: .stemmedGlassware,
+                        author: Author(person: AuthorNames.harryCraddock.rawValue, place: AuthorPlaces.savoy.rawValue, year: "1930"),
+                        spec: brunelleSpec,
+                        tags: brunelleTags)
+
+var brunelleSpec: [OldCocktailIngredient] = [OldCocktailIngredient(.otherAlcohol(.absinthe), value: 1),
+                                          OldCocktailIngredient(.juices(.lemon), value: 1),
+                                          OldCocktailIngredient(.syrups(.simple), value: 1)]
+
+var brunelleTags = Tags(profiles: [.citrusy, .refreshing, .light],
+                        styles: [.sour, .shaken])
+
+
