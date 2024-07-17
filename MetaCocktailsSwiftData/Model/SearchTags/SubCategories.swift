87837af2d8692e1f03dd6029da0fe8df57a96e60
James Menkal <gojamesgo85@gmail.com>
2024-07-17 17:27:08 -0600

    Added Spacing

--- MetaCocktailsSwiftData/Model/SearchTags/SubCategories.swift
+++ MetaCocktailsSwiftData/Model/SearchTags/SubCategories.swift
@@ -10,28 +10,28 @@ import Foundation
 
 enum SubCategories: String, Codable, CaseIterable {
     //Agave
-    case mezcalAny                 = "Mezcal"
-    case mezcalEspadin             = "Mezcal Espadine"
-    case mezcalSmokeyAny           = "Mezcal (Smokey)"
-    case mezcalNotSmokeyAny        = "Mezcal (Not Smokey)"
-    case tequilaAny                = "Tequila"
-    case tequilaAnejo              = "Tequila Anejo"
-    case tequilaBlanco             = "Tequila Blanco"
-    case tequilaReposado           = "Tequila Reposado"
+    case mezcalAny                    = "Mezcal"
+    case mezcalEspadin                = "Mezcal Espadine"
+    case mezcalSmokeyAny              = "Mezcal (Smokey)"
+    case mezcalNotSmokeyAny           = "Mezcal (Not Smokey)"
+    case tequilaAny                   = "Tequila"
+    case tequilaAnejo                 = "Tequila Anejo"
+    case tequilaBlanco                = "Tequila Blanco"
+    case tequilaReposado              = "Tequila Reposado"
     
     //Brandy
-    case appleBrandy          = "Apple Brandy"
-    case armagnac             = "Armagnac"
-    case cognacVSOP           = "Cognac (VSOP)"
-    case bondedApple          = "Bonded Apple Brandy"
-    case brandyAny            = "Brandy"
-    case pisco                = "Pisco"
-    case peruvianMustoVerde   = "Peruvian Musto Verde Pisco"
+    case appleBrandy                  = "Apple Brandy"
+    case armagnac                     = "Armagnac"
+    case cognacVSOP                   = "Cognac (VSOP)"
+    case bondedApple                  = "Bonded Apple Brandy"
+    case brandyAny                    = "Brandy"
+    case pisco                        = "Pisco"
+    case peruvianMustoVerde           = "Peruvian Musto Verde Pisco"
     
     //Gin
-    case ginAny               = "Gin"
-    case ginLondonDry         = "Gin (London Dry)"
-    case navyStrengthGin      = "Navy Strength Gin"
+    case ginAny                       = "Gin"
+    case ginLondonDry                 = "Gin (London Dry)"
+    case navyStrengthGin              = "Navy Strength Gin"
     
     //Other Alcohol
     //case absinthe                 = "Absinthe"
@@ -54,36 +54,36 @@ enum SubCategories: String, Codable, CaseIterable {
     case rumWhite                     = "Rum (White)"
     
     //Vodka
-    case vodkaAny                = "Vodka"
+    case vodkaAny                     = "Vodka"
     
     //Whisk(e)y
-    case americanWhiskeyAny    = "American Whiskey"
-    case bourbonAny            = "Bourbon"
-    case canadianWhiskeyAny    = "Canadian Whiskey"
-    case irishWhiskeyAny       = "Irish Whiskey"
-    case japaneseWhiskeyAny    = "Japanese Whiskey"
-    case ryeWhiskeyAny         = "Rye Whiskey"
-    case scotchAny             = "Scotch"
-    case scotchBlended         = "Scotch (Blended)"
-    case scotchHighland        = "Scotch (Highland)"
-    case scotchIsla            = "Scotch (Peated, From Isla)"
-    case whiskeyAny            = "Whisk(e)y"
+    case americanWhiskeyAny           = "American Whiskey"
+    case bourbonAny                   = "Bourbon"
+    case canadianWhiskeyAny           = "Canadian Whiskey"
+    case irishWhiskeyAny              = "Irish Whiskey"
+    case japaneseWhiskeyAny           = "Japanese Whiskey"
+    case ryeWhiskeyAny                = "Rye Whiskey"
+    case scotchAny                    = "Scotch"
+    case scotchBlended                = "Scotch (Blended)"
+    case scotchHighland               = "Scotch (Highland)"
+    case scotchIsla                   = "Scotch (Peated, From Isla)"
+    case whiskeyAny                   = "Whisk(e)y"
     
     //Fortified Wine
-    case amontillado            = "Amontillado Sherry"
-    case blancVermouth          = "Blanc Vermouth"
-    case dryVermouthAny         = "Dry Vermouth (French)"
-    case fino                   = "Fino Sherry"
-    case oloroso                = "Oloroso Sherry"
-    case sweetVermouthAny       = "Sweet Vermouth"
-    case tawnyPort              = "Tawny Port"
-    case vermouthAny            = "Vermouth"
-    case sherryAny              = "Sherry"
-    case fortifiedWineAny       = "Fortified Wine"
+    case amontillado                  = "Amontillado Sherry"
+    case blancVermouth                = "Blanc Vermouth"
+    case dryVermouthAny               = "Dry Vermouth (French)"
+    case fino                         = "Fino Sherry"
+    case oloroso                      = "Oloroso Sherry"
+    case sweetVermouthAny             = "Sweet Vermouth"
+    case tawnyPort                    = "Tawny Port"
+    case vermouthAny                  = "Vermouth"
+    case sherryAny                    = "Sherry"
+    case fortifiedWineAny             = "Fortified Wine"
     
     //Bitters
-    case bitters               = "Bitters"
+    case bitters                      = "Bitters"
     
     //Amaro
-    case amaroAny             = "Amaro"
+    case amaroAny                     = "Amaro"
 }
