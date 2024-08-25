33549eb63211169af459246800d942166e7e0e77
James Menkal <gojamesgo85@gmail.com>
2024-08-25 16:18:15 -0600

    Small change

---
+++ MetaCocktailsSwiftData/Views/Cocktail List View/SubViews/ListSearchBarView.swift
@@ -0,0 +1,19 @@
+//
+//  ListSearchBar.swift
+//  MetaCocktailsSwiftData
+//
+//  Created by James Menkal on 8/25/24.
+//
+
+import SwiftUI
+
+struct ListSearchBarView: View {
+    @Bindable var viewModel: CocktailListViewModel
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListSearchBarView(viewModel: CocktailListViewModel())
+}
