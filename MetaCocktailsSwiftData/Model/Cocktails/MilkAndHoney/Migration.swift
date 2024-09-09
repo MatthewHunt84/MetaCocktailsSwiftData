57443a45e537577780c2165ff79c193ae315958e
Matt Hunt <matthew.hunt@wundermanthompson.com>
2024-09-09 14:35:59 -0600

    Added Migration Cocktail

---
+++ MetaCocktailsSwiftData/Model/Cocktails/MilkAndHoney/Migration.swift
@@ -0,0 +1,27 @@
+//
+//  Migration.swift
+//  MetaCocktailsSwiftData
+//
+//  Created by Matt Hunt on 9/9/24.
+//
+
+import Foundation
+
+var migration = Cocktail(cocktailName: "Migration",
+                         glasswareType: .doubleOld,
+                         garnish: [.grapefruitPeel],
+                         ice: .bigRock,
+                         author: sashaPetraske,
+                         spec: migrationSpec,
+                         buildOrder: grapefruitCollinsBuild,
+                         tags: grapefruitCollinsTags,
+                         variation: .tomCollins,
+                         collection: .milkAndHoney)
+
+var migrationSpec  = [OldCocktailIngredient(.rums(.goslingsBlackSeal), value: 0.75),
+                      OldCocktailIngredient(.rums(.cruzanBlackstrap), value: 0.75),
+                      OldCocktailIngredient(.fortifiedWines(.cocchiDeTorino), value: 0.75),
+                      OldCocktailIngredient(.amari(.cynar), value: 0.75)]
+
+var migrationTags   = Tags(profiles: [.bitter, .spiritForward,],
+                           styles: [.stirred, .negroni])
