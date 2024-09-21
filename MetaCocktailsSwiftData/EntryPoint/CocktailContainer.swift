2f2267bccc1317f5f9ab1119f6d87e9422fff2bb
Matt Hunt <matthew.hunt@wundermanthompson.com>
2024-09-21 16:28:27 -0600

    Remove a bunch of now redundant code

--- MetaCocktailsSwiftData/EntryPoint/CocktailContainer.swift
+++ MetaCocktailsSwiftData/EntryPoint/CocktailContainer.swift
@@ -11,81 +11,32 @@ import SwiftData
 
 actor CocktailContainer {
     
-    @MainActor
-    static func preload(_ shouldPreload: inout Bool) -> ModelContainer {
-        let schema = Schema([Cocktail.self])
-        let config = ModelConfiguration()
+    static func useSwiftDataFile()  -> ModelContainer {
         
         do {
-            let container = try ModelContainer(for: schema, configurations: config)
-            
-            if shouldPreload {
-                print("🕔🕔🕔 PRELOADING COCKTAILS FOR FIRST TIME LAUNCH 🕔🕔🕔")
-                
-                insertCocktailsIntoModel(container: container)
-                
-                shouldPreload = false
-            } else {
-                print("✅✅✅ COCKTAILS LOADED FROM DATABASE ✅✅✅")
+            guard let bundleURL = Bundle.main.url(forResource: "default", withExtension: "store") else {
+                fatalError("💀 Error: Failed to find SwiftData default.store in app bundle - we can't run without it")
             }
+
+            let fileManager = FileManager.default
+            let documentDirectoryURL = try fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            let documentURL = documentDirectoryURL.appendingPathComponent("default.store")
+            
+            if !fileManager.fileExists(atPath: documentURL.path) {
+                print("✅ First Launch: Copying bundled sqlite file to documents")
+                try fileManager.copyItem(at: bundleURL, to: documentURL)
+            } else {
+                print("✅ Local SwiftData file found")
+            }
+            
+            let schema = Schema([Cocktail.self])
+            
+            let configuration = ModelConfiguration(url: documentURL)
+            let container = try! ModelContainer(for: schema, configurations: configuration)
             return container
             
         } catch {
-            fatalError("💀💀💀 MODEL CONTAINER FAILED TO INITIALIZE 💀💀💀")
-        }
-        
-        func insertCocktailsIntoModel(container: ModelContainer) {
-            // loop over cocktails
-            
-            let _ = Preload.allCases.map { $0.cocktails }
-                .flatMap { $0 }
-                .map { cocktail in
-                    
-                    // look at each cocktail's spec (array of ingredients)
-                    cocktail.spec.forEach { ingredient in
-                        
-                        let fetchDescriptor = FetchDescriptor<IngredientBase>(predicate: #Predicate { $0.name == ingredient.ingredientBase.name } )
-                        let existingBase = try? container.mainContext.fetch(fetchDescriptor).first
-                        // see if ingredientbase exists in model
-                        
-                        if let base = existingBase {
-                            // if yes - use existing ingredientbase
-                            ingredient.ingredientBase = base
-                        }
-                        // if no - add base as usual
-                    }
-                    
-                    // We also need to do the same for garnishes (so we don't violate the #unique name declaration)
-                    var garnishArray = [Garnish]()
-                    
-                    cocktail.garnish.forEach { garnish in
-                        
-                        let fetchDescriptor = FetchDescriptor<Garnish>(predicate: #Predicate { $0.name == garnish.name } )
-                        let existingGarnish = try? container.mainContext.fetch(fetchDescriptor).first
-                        
-                        if let foundGarnish = existingGarnish {
-                            garnishArray.append(foundGarnish)
-                        } else {
-                            garnishArray.append(garnish)
-                        }
-                    }
-                    
-                    cocktail.garnish = garnishArray
-                    
-                    // Add cocktail to context
-                    
-                    container.mainContext.insert(cocktail) }
-            
-            // Save context to container
-        }
-    }
-    
-    static func useSwiftDataFile()  -> ModelContainer {
-        do {
-            let storeURL = Bundle.main.url(forResource: "default", withExtension: "store")!
-            return try ModelContainer(for: Cocktail.self, configurations: ModelConfiguration(url: storeURL))
-        } catch {
-            fatalError("Failed to initialize ModelContainer: \(error)")
+            fatalError("💀 Error: \(error)")
         }
     }
 }
--- MetaCocktailsSwiftData/EntryPoint/MetaCocktailsSwiftDataApp.swift
+++ MetaCocktailsSwiftData/EntryPoint/MetaCocktailsSwiftDataApp.swift
@@ -11,141 +11,15 @@ import SwiftUI
 @main
 struct MetaCocktailsSwiftDataApp: App {
     @StateObject private var appState = AppState()
-    @AppStorage("shouldPreload") private var shouldPreload: Bool = true
-    var container = try? ModelContainer(for: Schema([Cocktail.self]), configurations: ModelConfiguration())
-    
-    init() {
-        
-        do {
-            guard let bundleURL = Bundle.main.url(forResource: "default", withExtension: "store") else {
-                fatalError("Failed to find SwiftData default.store in app bundle - we can't run without it")
-            }
-            
-            print("hello!")
-            let fileManager = FileManager.default
-            let documentDirectoryURL = try fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
-            let documentURL = documentDirectoryURL.appendingPathComponent("default.store")
-            
-            // Only copy the store from the bundle to the Documents directory if it doesn't exist
-            if !fileManager.fileExists(atPath: documentURL.path) {
-                try fileManager.copyItem(at: bundleURL, to: documentURL)
-            }
-            
-            let schema = Schema([Cocktail.self])
-            
-            let configuration = ModelConfiguration(url: documentURL)
-            self.container = try! ModelContainer(for: schema, configurations: configuration)
-        } catch {
-            debugPrint(error)
-        }
-    }
     
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .preferredColorScheme(.dark)
-                .modelContainer(container!)
-//                .modelContainer(CocktailContainer.preload(&shouldPreload))
+                .modelContainer(CocktailContainer.useSwiftDataFile())
                 .environmentObject(CBCViewModel())
                 .environmentObject(CocktailListViewModel())
                 .environmentObject(appState)
-        }
-    }
-    
-    func exportSwiftDataToJSON() {
-        do {
-            let container = try ModelContainer(for: Cocktail.self)
-            let context = container.mainContext
-            
-            let fetchDescriptor = FetchDescriptor<Cocktail>(sortBy: [SortDescriptor(\.cocktailName)])
-            let cocktails = try context.fetch(fetchDescriptor)
-            
-            let encoder = JSONEncoder()
-            encoder.outputFormatting = .prettyPrinted
-            let jsonData = try encoder.encode(cocktails)
-            
-            let fileManager = FileManager.default
-            let documentsDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
-            let fileURL = documentsDirectory.appendingPathComponent("ExportedCocktails.json")
-            
-            try jsonData.write(to: fileURL)
-            
-            print("✅✅✅ JSON file created successfully.")
-            print("File location: \(fileURL.path)")
-        } catch {
-            print("💀💀💀 Error exporting data to JSON: \(error)")
-        }
-    }
-    
-    func importJSONToSwiftData(_ shouldPreload: inout Bool) {
-        guard shouldPreload else {
-            print("✅ Models loaded from database")
-            return
-        }
-        
-        let modelImportStartTime = CACurrentMediaTime()
-        
-        let fileManager = FileManager.default
-        let documentsPath = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
-        let storePath = documentsPath.appendingPathComponent("Default.store")
-
-        
-        // Load JSON data from the app bundle
-        guard let jsonURL = Bundle.main.url(forResource: "Cocktails", withExtension: "json"),
-              let jsonData = try? Data(contentsOf: jsonURL) else {
-            print("Failed to load Cocktails.json file")
-            return
-        }
-        
-        do {
-            let decoder = JSONDecoder()
-            let cocktails = try decoder.decode([Cocktail].self, from: jsonData)
-            let container = try ModelContainer(for: Cocktail.self)
-            let context = container.mainContext
-            
-            for cocktail in cocktails {
-                
-                cocktail.spec.forEach { ingredient in
-                    
-                    let fetchDescriptor = FetchDescriptor<IngredientBase>(predicate: #Predicate { $0.name == ingredient.ingredientBase.name } )
-                    let existingBase = try? container.mainContext.fetch(fetchDescriptor).first
-                    // see if ingredientbase exists in model
-                    
-                    if let base = existingBase {
-                        // if yes - use existing ingredientbase
-                        ingredient.ingredientBase = base
-                    }
-                    // if no - add base as usual
-                }
-                
-                // We also need to do the same for garnishes (so we don't violate the #unique name declaration)
-                var garnishArray = [Garnish]()
-                
-                cocktail.garnish.forEach { garnish in
-                    
-                    let fetchDescriptor = FetchDescriptor<Garnish>(predicate: #Predicate { $0.name == garnish.name } )
-                    let existingGarnish = try? container.mainContext.fetch(fetchDescriptor).first
-                    
-                    if let foundGarnish = existingGarnish {
-                        garnishArray.append(foundGarnish)
-                    } else {
-                        garnishArray.append(garnish)
-                    }
-                }
-                
-                cocktail.garnish = garnishArray
-
-                context.insert(cocktail)
-                print("Added: \(cocktail.cocktailName)")
-            }
-            
-            try context.save()
-            shouldPreload = false
-            print("🕔Loading Time: \(CACurrentMediaTime() - modelImportStartTime)")
-            print("✅ Cocktails imported from JSON successfully. (🕔Loading Time: \(CACurrentMediaTime() - modelImportStartTime))")
-            
-        } catch {
-            print("💀 Failed to import cocktails: \(error)")
         }
     }
 }
@@ -154,11 +28,6 @@ struct MetaCocktailsSwiftDataApp: App {
 class AppState: ObservableObject {
     @Published var isDataReady = false
     @Published var showMainContent = false
-    private var loadingStartTime: Date?
-    
-    init() {
-        loadingStartTime = Date()
-    }
     
     func setDataReady() {
         isDataReady = true
@@ -198,6 +67,7 @@ struct ContentView: View {
         }
     }
 }
+
 extension ModelContext {
     var sqliteCommand: String {
         if let url = container.configurations.first?.url.path(percentEncoded: false) {
