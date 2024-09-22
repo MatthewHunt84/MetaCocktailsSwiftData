6d34e2fdd88e83d6556946e735c744b1fb588735
Matt Hunt <matthew.hunt@wundermanthompson.com>
2024-09-22 02:12:08 -0600

    Moved search info view into info icon

--- MetaCocktailsSwiftData/Views/Search View/Ingredient Search View/Subviews/FindCocktailUserInfoView.swift
+++ MetaCocktailsSwiftData/Views/Search View/Ingredient Search View/Subviews/FindCocktailUserInfoView.swift
@@ -8,13 +8,30 @@
 import SwiftUI
 
 struct FindCocktailUserInfoView: View {
+    
+    @State var isShowingSearchInfo = false
+    
     var body: some View {
         VStack(alignment: .leading) {
-            Text("Find cocktails by searching their ingredients")
-                .font(FontFactory.regularFont(size: 18))
-            Text("Choose any ingredient, category, flavor, style or profile to be included or excluded from cocktail results")
-                .font(FontFactory.regularFont(size: 16))
-                .foregroundStyle(.secondary)
+            
+            HStack {
+                Text("Search for cocktails by ingredient")
+                    .font(FontFactory.regularFont(size: 18))
+                
+                    Image(systemName: "info.circle")
+                    .foregroundStyle(isShowingSearchInfo ? ColorScheme.tintColor : ColorScheme.interactionTint)
+                        .onTapGesture {
+                            withAnimation(.easeInOut(duration: 0.5)) {
+                                isShowingSearchInfo.toggle()
+                            }
+                        }
+            }
+            
+            if isShowingSearchInfo {
+                Text("Choose any ingredient, category, flavor, style or profile to be included or excluded from cocktail results")
+                    .font(FontFactory.regularFont(size: 16))
+                    .foregroundStyle(.secondary)
+            }
         }
         .padding(.leading, 16)
         .padding(.trailing, 16)
