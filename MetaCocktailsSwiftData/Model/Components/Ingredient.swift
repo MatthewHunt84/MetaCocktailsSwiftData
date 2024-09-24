eb89647a309d9165043f3f256d92985518b2b5a7
James Menkal <gojamesgo85@gmail.com>
2024-09-23 23:37:46 -0600

    Shortened bitters titles

--- MetaCocktailsSwiftData/Model/Components/Ingredient.swift
+++ MetaCocktailsSwiftData/Model/Components/Ingredient.swift
@@ -132,7 +132,7 @@ class OldCocktailIngredient: Codable, Hashable { // This old guy needs to be rem
                 return "French bitter apertif"
             }
             if ingredient.name == Bitters.bitterTruthJTDecanter.rawValue {
-                return "By Bitter Truth"
+                return "Bitters by Bitter Truth"
             }
             if ingredient.name == Bitters.blackWalnut.rawValue {
                 return "By Fee Brothers"
@@ -148,6 +148,9 @@ class OldCocktailIngredient: Codable, Hashable { // This old guy needs to be rem
             }
             if ingredient.name ==  Liqueur.luxardoCherry.rawValue {
                 return "Liqueur by Luxardo"
+            }
+            if ingredient.name ==  Bitters.bittercubeJamaican1.rawValue {
+                return "Bitters by Bittercube"
             }
             return info
         }()
