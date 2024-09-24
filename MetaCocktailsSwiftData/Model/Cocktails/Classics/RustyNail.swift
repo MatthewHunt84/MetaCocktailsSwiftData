c1a532b3ffe6ad0eed43132688c7a9bbce8ff9a4
James Menkal <gojamesgo85@gmail.com>
2024-09-24 00:35:26 -0600

    Changed Rusty Nail note

--- MetaCocktailsSwiftData/Model/Cocktails/Classics/RustyNail.swift
+++ MetaCocktailsSwiftData/Model/Cocktails/Classics/RustyNail.swift
@@ -24,4 +24,4 @@ var rustyNailSpec     =  [OldCocktailIngredient(.liqueurs(.drambuie), value: 1),
 var rustyNailTags     = Tags(profiles: [.spiritForward, .punchy, .sweet],
                              styles: [.oldFashioned, .stirred])
 
-var rustyNailNotes = "Dave Wondrich says that the name 'Rusty Nail' wasn't recorded until 1961 and that the combination of Drambuie and Scotch is probably as old as when Drambuie was produced (1908). Dave is the man to listen to for anythin cocktail history related, so we'll listen to him. That's the reaon we aren't showing an author here."
+var rustyNailNotes = "According to cocktail historian Dave Wondrich, the name 'Rusty Nail' wasn't documented until 1961. However, the combination of Drambuie and Scotch likely dates back to 1908 when Drambuie began production. Given Wondrich's reputation as a leading authority on cocktail history, his research on this topic is considered highly reliable. The absence of a specific creator attribution for the Rusty Nail reflects the drink's unclear origins and the general consensus among experts about its history."
