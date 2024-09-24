06e55b416a5b8a637ed1ab80b74ed7bfb222066c
James Menkal <gojamesgo85@gmail.com>
2024-09-23 21:58:50 -0600

    Added Corn n Oli notes

--- MetaCocktailsSwiftData/Model/Cocktails/Classics/CornNOil.swift
+++ MetaCocktailsSwiftData/Model/Cocktails/Classics/CornNOil.swift
@@ -8,18 +8,21 @@
 import Foundation
 
 var cornNOil  = Cocktail(cocktailName: "Corn 'n' Oil",
-                        glasswareType: .doubleOld,
-                        garnish: [.noGarnish],
+                         glasswareType: .doubleOld,
+                         garnish: [.noGarnish],
                          ice: .crackedIce,
-                        spec: cornNOilSpec,
-                        tags: cornNOilTags)
+                         spec: cornNOilSpec,
+                         notes: cornNOilNotes,
+                         tags: cornNOilTags,
+                         collection: .originals)
 
 var cornNOilSpec  = [OldCocktailIngredient(.bitters(.angosturaBitters), value: 3, unit: .dashes),
                      OldCocktailIngredient(.juices(.lime), value: 0.5),
                      OldCocktailIngredient(.liqueurs(.falernum), value: 0.5),
-                    OldCocktailIngredient(.rums(.rumBlackStrap), value: 2)]
+                     OldCocktailIngredient(.rums(.rumBlackStrap), value: 2)]
 
 
 var cornNOilTags   = Tags(profiles: [.citrusy, .rich],
-                         styles: [.sour, .shaken])
+                          styles: [.sour, .shaken])
                       
+var cornNOilNotes = "The Corn 'n' Oil cocktail likely originated in Barbados during the 18th century. Its intriguing name is derived from a biblical passage in Deuteronomy, which refers to gathering 'corn, wine, and oil.'Like other cocktails with such a long history, it fails to produce a noteable author."
