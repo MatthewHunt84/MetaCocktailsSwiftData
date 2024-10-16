acfe1632b084422c46346f7fd6ebef9555170a14
Matt Hunt <matthew.hunt@wundermanthompson.com>
2024-10-16 13:04:03 -0600

    add missing files

---
+++ MetaCocktailsSwiftData/Model/Cocktails/Historic/Algonquin(1935).swift
@@ -0,0 +1,27 @@
+//
+//  Algonquin(1935).swift
+//  MetaCocktailsSwiftData
+//
+//  Created by James Menkal on 2/1/24.
+//
+
+import Foundation
+
+var algonquin  = Cocktail(cocktailName: "Algonquin" + historicTag,
+                          glasswareType: .martini,
+                          garnish: [.noGarnish],
+                          author: Author(person: "G. Selmer Foughner", place: "Along The Wine Trail: an anthology of wine and spirits", year: "1935"),
+                          spec: algonquinSpec,
+                          tags: algonquinTags,
+                          variation: .algonquin,
+                          collection: .originals,
+                          titleCocktail: true,
+                          historicSpec: .algonquin)
+
+var algonquinSpec  = [OldCocktailIngredient(.juices(.pineappleJuice), value: 1),
+                      OldCocktailIngredient(.fortifiedWines(.dryVermouthAny), value: 1),
+                      OldCocktailIngredient(.whiskies(.ryeWhiskeyAny), value: 2)]
+
+var algonquinTags   = Tags(profiles: [.fruity, .light, .dry],
+                           styles: [.shaken, .sour])
+
---
+++ MetaCocktailsSwiftData/Model/Cocktails/Historic/ChocolateCocktail.swift
@@ -0,0 +1,27 @@
+//
+//  ChocolateCocktail(W&G).swift
+//  MetaCocktailsSwiftData
+//
+//  Created by James Menkal on 1/23/24.
+//
+
+import Foundation
+
+var chocolateCocktail  = Cocktail(cocktailName: "Chocolate Cocktail No. 2" + historicTag,
+                                  glasswareType: .singleOld,
+                                  author:Author(person: AuthorNames.harryCraddock.rawValue, place: AuthorPlaces.savoy.rawValue, year: "1930"),
+                                  spec: chocolateCocktailSpec,
+                                  buildOrder: eggWhiteInstructionsNoBitters,
+                                  tags: chocolateCocktailTags,
+                                  variation: .chocolateCocktail,
+                                  collection: .originals,
+                                  historicSpec: .chocolateCocktail)
+
+var chocolateCocktailSpec  = [OldCocktailIngredient(.otherNonAlc(.eggYolk), value: 1, unit: .whole),
+                              OldCocktailIngredient(.otherNonAlc(.crushedChocolate), value: 1, unit: .teaspoon),
+                              OldCocktailIngredient(.liqueurs(.yellowChartreuse), value: 0.5),
+                              OldCocktailIngredient(.fortifiedWines(.tawnyPort) , value: 1.5)]
+
+var chocolateCocktailTags   = Tags(profiles: [.sweet, .rich],
+                                   styles: [.shaken, .flip])
+
